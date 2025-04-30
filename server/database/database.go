0631031b6a1c09517382fc616ec8fbbbe6986ed6
ParthaX09 <parthax09@gmail.com>
2025-04-30 12:39:30 +0530

    made Mysql dsn dynamic

--- server/database/database.go
+++ server/database/database.go
@@ -1,16 +1,35 @@
 package database
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func Connect() error {
-    var err error
-    dsn := "root:Cbnits@123@tcp(localhost:3306)/user"
+
+    // var err error
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+
+    // Retrieve environment variables
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    dbname := os.Getenv("DB_NAME")
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, host, port, dbname)
+    // dsn := "root:Cbnits@123@tcp(localhost:3306)/user"
+    
     DB, err = sql.Open("mysql", dsn)
     if err != nil {
         return fmt.Errorf("error opening database: %v", err)
